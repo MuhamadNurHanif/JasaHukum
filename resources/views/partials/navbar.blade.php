910decc4bc0787d632ca469c7fb317171569c485
Muhamad Nur Hanif <114205300+MuhamadNurHanif@users.noreply.github.com>
2025-04-21 13:59:18 +0700

    fix: ui pages

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -70,7 +70,7 @@
             <div class="flex items-center space-x-4">
                 <div class="flex items-center">
                     <i class="fas fa-balance-scale text-2xl mr-2 text-blue-300"></i>
-                    <span class="font-bold text-xl">Tri Sulasmono, S.H. & Partner</span>
+                    <span class="font-bold text-xl">Tri Sulasmono S.H. & Partner</span>
                 </div>
             </div>
             <div class="hidden md:flex items-center space-x-8">
@@ -81,8 +81,10 @@
             </div>
             <div class="md:hidden flex items-center">
                 <button class="outline-none mobile-menu-button">
-                    <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </button>
             </div>
