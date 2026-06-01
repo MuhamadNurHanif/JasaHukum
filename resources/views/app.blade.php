85fc1a1935a5184362ac54ba145739abcb96361d
jabiralawfaa <jadealtero@gmail.com>
2026-06-01 20:27:48 +0700

    SEO optimization: meta tags, structured data, sitemap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,30 +2,111 @@
 <html lang="id">
 
 <head>
-    <link rel="canonical" href="https://demo.themesberg.com/landwind/" />
+    <link rel="canonical" href="https://jasa-hukum.vercel.app/" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda - Jasa Bantuan Hukum</title>
+    <title>Tri Sulasmono, S.H. & Partner - Jasa Hukum Banyuwangi | Advokat Terpercaya</title>
 
     <!-- Meta SEO -->
-    <meta name="title" content="Beranda - Jasa Bantuan Hukum">
-    <meta name="description" content="Kami melayani secar professional dan Cepat">
+    <meta name="title" content="Tri Sulasmono, S.H. & Partner - Jasa Hukum Banyuwangi | Advokat Terpercaya">
+    <meta name="description" content="TRI SULASMONO, S.H. & Partner adalah kantor hukum profesional di Banyuwangi yang melayani konsultasi hukum, litigasi, non-litigasi, pendampingan hukum, dan pengurusan perizinan. Hubungi kami untuk layanan advokat terpercaya di Banyuwangi.">
+    <meta name="keywords" content="tri sulasmono, jasa hukum banyuwangi, advokat banyuwangi, pengacara banyuwangi, kantor hukum banyuwangi, konsultasi hukum, litigasi, non-litigasi, bantuan hukum, pengurusan izin, legal opinion, due diligence, hukum pidana, hukum perdata, hukum keluarga">
     <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="Themesberg">
+    <meta name="language" content="Indonesian">
+    <meta name="author" content="Tri Sulasmono, S.H. & Partner">
+    <meta name="geo.region" content="ID-JI">
+    <meta name="geo.placename" content="Banyuwangi">
 
     <!-- Social media share -->
-    <meta property="og:title" content="Beranda - Jasa Bantuan Hukum">
-    <meta property="og:site_name" content="Themesberg">
+    <meta property="og:title" content="Tri Sulasmono, S.H. & Partner - Jasa Hukum Banyuwangi">
+    <meta property="og:site_name" content="Tri Sulasmono, S.H. & Partner">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:description" content="Jasa bantuan hukum terpercaya dan profesional.">
+    <meta property="og:description" content="Kantor hukum profesional di Banyuwangi. Melayani konsultasi hukum, litigasi, non-litigasi, pendampingan hukum, dan pengurusan perizinan dengan integritas tinggi.">
     <meta property="og:type" content="website">
+    <meta property="og:locale" content="id_ID">
     <meta property="og:image" content="{{ asset('images/logo2-bulat.png') }}">
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@themesberg" />
-    <meta name="twitter:creator" content="@themesberg" />
+    <meta property="og:image:alt" content="Logo Tri Sulasmono S.H. & Partner - Kantor Hukum Banyuwangi">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@trisulasmono" />
+    <meta name="twitter:creator" content="@trisulasmono" />
+    <meta name="twitter:title" content="Tri Sulasmono, S.H. & Partner - Jasa Hukum Banyuwangi" />
+    <meta name="twitter:description" content="Kantor hukum profesional di Banyuwangi. Melayani konsultasi hukum, litigasi, non-litigasi, dan pendampingan hukum." />
+    <meta name="twitter:image" content="{{ asset('images/logo2-bulat.png') }}" />
+
+    <!-- JSON-LD Structured Data for LocalBusiness -->
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "LegalService",
+      "name": "TRI SULASMONO, S.H. & PARTNER",
+      "alternateName": "Tri Sulasmono Law Firm Banyuwangi",
+      "description": "Kantor hukum profesional di Banyuwangi yang melayani konsultasi hukum, litigasi, non-litigasi, pendampingan hukum, dan pengurusan perizinan dengan integritas tinggi.",
+      "url": "https://jasa-hukum.vercel.app/",
+      "telephone": "+62-859-1069-53875",
+      "email": "lawfirmthree@gmail.com",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Dusun Krajan RT.001 RW.009 Desa Dadapan Kecamatan Kabat",
+        "addressLocality": "Banyuwangi",
+        "addressRegion": "Jawa Timur",
+        "postalCode": "68461",
+        "addressCountry": "ID"
+      },
+      "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": "-8.259065",
+        "longitude": "114.34282"
+      },
+      "areaServed": {
+        "@type": "GeoCircle",
+        "geoMidpoint": {
+          "@type": "GeoCoordinates",
+          "latitude": "-8.259065",
+          "longitude": "114.34282"
+        },
+        "geoRadius": "50000"
+      },
+      "openingHoursSpecification": {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday"
+        ],
+        "opens": "08:00",
+        "closes": "17:00"
+      },
+      "priceRange": "Rp$$",
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.8",
+        "reviewCount": "99",
+        "bestRating": "5",
+        "worstRating": "1"
+      },
+      "founder": {
+        "@type": "Person",
+        "name": "Tri Sulasmono",
+        "honorificPrefix": "S.H.",
+        "jobTitle": "Advokat / Pengacara",
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Banyuwangi",
+          "addressRegion": "Jawa Timur",
+          "addressCountry": "ID"
+        }
+      },
+      "sameAs": [
+        "https://www.facebook.com/share/1CQjGcxujy/",
+        "https://wa.me/62859106953875"
+      ]
+    }
+    </script>
 
     <!-- Favicon -->
     <link rel="icon" type="image/jpeg" href="{{ asset('images/logo2-bulat.png') }}">
